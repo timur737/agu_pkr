229e590a626a597d7d3e89c9b8ab1b48a18e569d
Timur Obolbekov <56782741+timur737@users.noreply.github.com>
2026-08-16 23:18:12 +0600

    Allow saving pages with translated admin blocks

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.db import models
-from modeltranslation.admin import TabbedTranslationAdmin, TranslationTabularInline
+from django.utils.html import format_html
+from modeltranslation.admin import TabbedTranslationAdmin, TranslationStackedInline
 from .models import AdminPage, News, PageBlock
 from .widgets import CKEditorTextarea
 
@@ -11,19 +12,26 @@ class DescriptionCKEditorMixin:
     }
 
 
-class PageBlockInline(DescriptionCKEditorMixin, TranslationTabularInline):
+class PageBlockInline(DescriptionCKEditorMixin, TranslationStackedInline):
     model = PageBlock
-    extra = 1
+    # Empty inline forms used to be submitted as a block with order=0.  Blocks
+    # must only be created after the editor explicitly presses "Add another".
+    extra = 0
     fields = ('order', 'block_type', 'title', 'description', 'date', 'photo', 'file', 'url', 'value', 'is_active')
+
+    class Media:
+        css = {'all': ('app/admin/content_structure.css',)}
+        js = ('app/admin/page_blocks.js',)
 
 
 @admin.register(AdminPage)
 class AdminPageAdmin(DescriptionCKEditorMixin, TabbedTranslationAdmin):
-    list_display = ('order', 'title', 'group', 'parent', 'is_development', 'is_active')
+    list_display = ('order', 'structured_title', 'group', 'parent', 'is_development', 'is_active')
     list_filter = ('group', 'is_development', 'is_active')
     search_fields = ('title', 'description', 'slug')
-    list_display_links = ('title',)
+    list_display_links = ('structured_title',)
     list_editable = ('order',)
+    ordering = ('order', 'id')
     prepopulated_fields = {'slug': ('title_ru',)}
     inlines = [PageBlockInline]
     fieldsets = (
@@ -31,18 +39,14 @@ class AdminPageAdmin(DescriptionCKEditorMixin, TabbedTranslationAdmin):
         ('Контент страницы', {'fields': ('main_photo', 'description', 'redirect_url', 'pdf_file')}),
     )
 
+    @admin.display(description='Страница', ordering='order')
+    def structured_title(self, obj):
+        """Make the page/subpage relationship visible in the page list."""
+        prefix = '↳ ' if obj.parent_id else ''
+        return format_html('<span class="admin-page-depth-{}">{}{}</span>', 1 if obj.parent_id else 0, prefix, obj.title)
 
-@admin.register(PageBlock)
-class PageBlockAdmin(DescriptionCKEditorMixin, TabbedTranslationAdmin):
-    list_display = ('order', 'title', 'page', 'block_type', 'is_active')
-    list_filter = ('page__group', 'page', 'block_type', 'is_active')
-    search_fields = ('title', 'description', 'url', 'value')
-    list_display_links = ('title',)
-    list_editable = ('order',)
-    fieldsets = (
-        ('Основное', {'fields': ('page', 'order', 'block_type', 'title', 'description', 'is_active')}),
-        ('Медиа, ссылки и значения', {'fields': ('date', 'photo', 'file', 'url', 'value')}),
-    )
+    def get_queryset(self, request):
+        return super().get_queryset(request).select_related('parent')
 
 
 @admin.register(News)
